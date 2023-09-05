081554e168274db13de6f2f00895c13e14db4a7a
Agwam Ayatullah <agwamayatullah@gmail.com>
2023-09-05 16:47:14 +0800

    archive my study about ruby

---
+++ ruby_study_1/page7/index.rb
@@ -0,0 +1,9 @@
+text = "Mari belajar pemrograman menggunakan Progate"
+# Gabungkan variable text dan string yang telah diberikan, lalu cetak hasilnya
+puts text + ' menggunakan Progate'
+
+length = 8
+width = 9
+
+# Cetak hasil dari perkalian length * width
+puts length * width
---
+++ ruby_study_5/page2/drink.rb
@@ -0,0 +1,6 @@
+# Lakukan import `menu.rb` menggunakan `require`
+require './menu'
+
+# Definisikan class `Food` yang mewarisi class `Menu`
+class Drink < Menu
+end
